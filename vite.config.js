093bc15ccdd4088998d7963617db06ab7afbb821
amnhmb <130522676+amnhmb@users.noreply.github.com>
2026-08-19 20:45:14 +0800

    31 LAB CATALOG V3

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
+        about: resolve(__dirname, 'src/about.html'),
+        contact: resolve(__dirname, 'src/contact.html'),
         admin: resolve(__dirname, 'src/admin/index.html'),
       },
     },
