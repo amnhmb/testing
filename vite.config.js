a468172a0c80de88c4a4c22d565144ebf6124935
amnhmb <130522676+amnhmb@users.noreply.github.com>
2026-08-19 20:28:46 +0800

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: process.env.CF_PAGES ? '/' : '/thirtyonelab.catalog/',
+  base: process.env.CF_PAGES ? '/' : '/testing/',
   root: 'src',
   envDir: '../',
   publicDir: '../public',
